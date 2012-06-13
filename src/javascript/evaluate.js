b72140af86476f5dbe7da62e568c8f903a6a0db0
Matt Fenwick <mfenwick100@gmail.com>
2012-06-13 16:15:45 -0400

    add primitive data types and some primitive functions; add eval; unit tests for both

--- src/javascript/evaluate.js
+++ src/javascript/evaluate.js
@@ -1,6 +1,43 @@
 
 var evaluate = (function() {
 
+function MyNumber(value) {
+  this.value = value;
+  this.type = 'number';
+}
+
+
+function MyString(value) {
+  this.value = value;
+  this.type = 'string';
+}
+
+
+function Symbol(value) {
+  this.value = value;
+  this.type = 'symbol';
+}
+
+
+function List(value) {
+  this.value = value;
+  this.type = 'list';
+}
+
+
+function MyFunction(value) {
+  this.value = value;
+  this.type = 'function';
+}
+
+
+function Nil() {
+  this.value = false;
+  this.type = 'nil';
+}
+
+
+
 var INTEGER = /^\d+$/;
 
 var FLOAT = /^(?:\d*\.\d+|\d+\.\d*)$/;
@@ -11,58 +48,147 @@ function makePrimitives(sexpr) {
   var i, value, elems;
 
   if(typeof(sexpr) === "string") {
-    if( value = sexpr.match(INTEGER) ) {
-      return {
-        'type': 'number',
-        'value': Number(value[0])
-      };
-    } else if( value = sexpr.match(FLOAT) ) {
-      return {
-        'type': 'number',
-        'value': Number(value[0])
-      };
-    } else if( value = sexpr.match(STRING) ) {
-      return {
-        'type': 'string',
-        'value': value[1] // the second value is the match *without* the " marks
-      };
-    } else if( sexpr.length > 0 ) {
-      return {
-        'type': 'symbol',
-        'value': sexpr
-      };
-    } else { // empty string -> Error
+      if( value = sexpr.match(INTEGER) ) {
+          return new MyNumber(Number(value[0]));
+      } 
+    
+      if( value = sexpr.match(FLOAT) ) {
+          return new MyNumber(Number(value[0]));
+      } 
+    
+      if( value = sexpr.match(STRING) ) {
+          return new MyString(value[1]); // the second value is the match *without* the " marks
+      }
+    
+      if( sexpr.length > 0 ) {
+          return new Symbol(sexpr);
+      } 
+    
+      // empty string -> Error
       throw new Error("can't extract primitive:  empty value");
-    }
-  } else { // assume it's a list/array
-    elems = [];
-    
-    for(i = 0; i < sexpr.length; i++) {
-      elems.push(makePrimitives(sexpr[i]));
-    }
-    
-    return {
-      'type': 'list',
-      'value': elems
-    };
   }
+  
+  // assume it's a list/array
+  elems = [];
+    
+  for(i = 0; i < sexpr.length; i++) {
+    elems.push(makePrimitives(sexpr[i]));
+  }
+    
+  return new List(elems);
 }
 
 
-function myapply() {
+function cons(elem, list) {
+  var newList = [elem];
+  for(var i = 0; i < list.value.length; i++) {
+    newList.push(list.value[i]);
+  }
+  return new List(newList);
+};
 
+
+function car(list) {
+  if( list.value.length > 0 ) {
+    return list.value[0];
+  }
+  
+  return new Nil();
 }
 
 
-function evaluate(sexpr) {
-//  if(sexpr
+function cdr(list) {
+  if( list.value.length == 0 ) {
+    return new Nil();
+  }
+  
+  return new List(list.value.slice(1));
+}
 
+
+function list() {
+  var args = [];
+  for(var i = 0; i < arguments.length; i++) {
+    args.push(arguments[i]);
+  }
+  return new List(args);
+}
+
+
+var environment = {
+
+  'cons': new MyFunction(cons),
+  
+  'car': new MyFunction(car),
+  
+  'cdr': new MyFunction(cdr),
+  
+  'list': new MyFunction(list)
+  
+};
+
+
+function myapply(f, args) {
+  return f.apply(null, args);
+}
+
+
+
+function evaluate(sexpr, env) {
+  if( sexpr.type === 'list' ) {
+    // what if it's empty?
+    if( !sexpr.value[0] ) {
+      throw new Error("cannot evaluate empty list");
+    }
+    
+    var first = evaluate(sexpr.value[0], env);
+    
+    if( first.type !== 'function' ) {
+      throw new Error("first element in list must be function");
+    }
+    
+    var args = sexpr.value.slice(1);
+
+    var evaledArgs = args.map(function(a) {
+        return evaluate(a, env);
+    });
+
+    var func = first.value;
+    var val = myapply(func, evaledArgs);
+    return val;
+  }
+  
+  if ( sexpr.type === 'symbol' ) {
+    if( sexpr.value in env ) { // uh ... has own property?
+      return env[sexpr.value];
+    } else {
+      throw new Error("could not find symbol " + sexpr.value);
+    }
+  } 
+  
+  if ( sexpr.type === 'number' || sexpr.type === 'string' ) {
+    return sexpr;
+  } 
+
+  throw new Error("unrecognized type: " + sexpr.type);
+}
+
+
+function evalHead(sexpr) {
+  return evaluate(sexpr, environment);
 }
 
 
 return {
-  'makePrimitives': makePrimitives,
-  'apply'  : myapply,
-  'eval' : evaluate
+  'makePrimitives' : makePrimitives,
+  'apply'          : myapply,
+  'eval'           : evalHead,
+  'environment'    : environment,
+  'Number'         : function(x) {return new MyNumber(x)},
+  'String'         : function(x) {return new MyString(x)},
+  'Function'       : function(x) {return new MyFunction(x)},
+  'List'           : function(x) {return new List(x)},
+  'Symbol'         : function(x) {return new Symbol(x)},
+  'Nil'            : function(x) {return new Nil(x)}
 };
 })();
--- src/javascript/test_evaluate.js
+++ src/javascript/test_evaluate.js
@@ -1,6 +1,8 @@
 
 function testEvaluate(evaluate) {
 
+    var ev = evaluate;
+
     module("primitives");
 
     test("makePrimitives", function() {
@@ -16,51 +18,96 @@ function testEvaluate(evaluate) {
           list1 = ["+", '"str1"', "345"],
           list2 = ["+", ["-", "34.32"], '"omg'];
           
-      deepEqual({'type': 'number', 'value': 345}, evaluate.makePrimitives(int_));
+      deepEqual(ev.Number(345), ev.makePrimitives(int_));
           
       try {
-        var e = evaluate.makePrimitives(empty);
+        var e = ev.makePrimitives(empty);
         ok(0);
       } catch(e) {
         ok(1, "can't make primitive of empty string");
       };
           
-      deepEqual({'type': 'number', 'value': 3}, evaluate.makePrimitives(float1));
+      deepEqual(ev.Number(3), ev.makePrimitives(float1));
           
-      deepEqual({'type': 'number', 'value': 3.456}, evaluate.makePrimitives(float2));
+      deepEqual(ev.Number(3.456), evaluate.makePrimitives(float2));
           
-      deepEqual({'type': 'number', 'value': 0.001}, evaluate.makePrimitives(float3));
+      deepEqual(ev.Number(0.001), evaluate.makePrimitives(float3));
           
-      deepEqual({'type': 'number', 'value': 0.01}, evaluate.makePrimitives(float4));
+      deepEqual(ev.Number(0.01), evaluate.makePrimitives(float4));
           
-      deepEqual({'type': 'string', 'value': "yes this is a string"}, evaluate.makePrimitives(str));
+      deepEqual(ev.String("yes this is a string"), ev.makePrimitives(str));
           
-      deepEqual({'type': 'symbol', 'value': '"open'}, evaluate.makePrimitives(notstr));
+      deepEqual(ev.Symbol('"open'), evaluate.makePrimitives(notstr));
           
-      deepEqual({'type': 'symbol', 'value': '*?#""/'}, evaluate.makePrimitives(sym1));
+      deepEqual(ev.Symbol('*?#""/'), evaluate.makePrimitives(sym1));
           
-      var l1 = {
-        'type': 'list', 
-        'value': [
-          {'type': 'symbol', 'value': '+'},
-          {'type': 'string', 'value': 'str1'},
-          {'type': 'number', 'value': 345}
-        ]
-      };
-      deepEqual(l1, evaluate.makePrimitives(list1));
+      var l1 = ev.List([ev.Symbol('+'), ev.String('str1'), ev.Number(345)]);
+      deepEqual(l1, ev.makePrimitives(list1));
           
-      var l2 = {
-        'type': 'list', 
-        'value': [
-          {'type': 'symbol', 'value': '+'},
-          {'type': 'list', 'value': [
-            {'type': 'symbol', 'value': '-'},
-            {'type': 'number', 'value': 34.32}
-          ]},
-          {'type': 'symbol', 'value': '"omg'}
-        ]
-      };          
+      var l2 = ev.List([
+          ev.Symbol('+'),
+          ev.List([ev.Symbol('-'), ev.Number(34.32)]),
+          ev.Symbol('"omg')
+      ]);
       deepEqual(l2, evaluate.makePrimitives(list2));
+    });
+    
+    
+    
+
+    module("evaluate");
+
+    test("cons, list, car, cdr", function() {
+    
+      deepEqual(3, evaluate.environment.car.value(ev.List([3, 4])));
+    
+      // uh-oh, empty list!
+      deepEqual(ev.Nil(), evaluate.environment.car.value(ev.List([])));
+    
+      deepEqual(
+          ev.List([4, 10, 'hello']), 
+          evaluate.environment.cdr.value(ev.List([3, 4, 10, 'hello']))
+      );
+    
+      // uh-oh !!
+      deepEqual(ev.Nil(), evaluate.environment.cdr.value(ev.List([])));
+      
+      deepEqual(ev.List([3, 4, 5]), ev.environment.list.value(3, 4, 5));
+      
+    });
+    
+    test("evaluate", function() {
+      var int_ = ev.Number(31),
+          str = ev.String("abcde"),
+          sym = ev.Symbol('cons'),
+          l1 = ev.List([
+              ev.Symbol('car'),
+              ev.List([ev.Symbol('list'), ev.Number(87)])
+          ]),
+          l2 = ev.List([
+              ev.Symbol('cons'),
+              ev.String('what?'),
+              ev.List([ev.Symbol('list')])
+          ])
+          ;
+          
+      deepEqual(ev.Number(31), evaluate.eval(int_));
+          
+      deepEqual(ev.String("abcde"), evaluate.eval(str));
+          
+      deepEqual(ev.Function(evaluate.environment.cons.value), evaluate.eval(sym));
+    
+      deepEqual(ev.List([]), ev.eval(ev.List([ev.Symbol('list')])));
+    
+      deepEqual(ev.List([ev.Number(4)]), ev.eval(ev.List([ev.Symbol('list'), ev.Number(4)])));
+          
+      deepEqual(
+        ev.Number(87),
+        evaluate.eval(l1)
+      );
+          
+      deepEqual(ev.List([ev.String('what?')]), evaluate.eval(l2));
+      
     });
 
 }
