03c221cb5307c022f278bf0b0dd8b2772a1a6127
Matt Fenwick <mfenwick100@gmail.com>
2012-06-19 21:02:27 -0400

    refactor Parse:
     - define data types for Tokens, s-expressions, and parse errors
    
     - correctly parse strings that include whitespace
    
     - add support for tokenizing comments
    
     - improve error reporting

--- src/javascript/parse.js
+++ src/javascript/parse.js
@@ -1,12 +1,61 @@
 
 var Parse = (function() {
 "use strict";
+
+
+// Token types: open, close, symbol, string, comment
+function Token(type, value) {
+  this.type = type;
+  this.value = value;
+}
+
+
+// SEexpression types: symbol, string, list
+function SExpression(type, value) {
+  this.type = type;
+  this.value = value;
+}
+
+
+function ParseError(message, value) {
+  this.message = message;
+  this.value = value;
+}
+
 	
-var SYMBOL = /^[^\s\(\)]+/,
-    OPEN = "(",
-    CLOSE = ")";
+var SYMBOL  = /^[^\s\(\)"]+/, /* not ws, (, ), or " */
+    OPEN    = "(",
+    CLOSE   = ")",
+    COMMENT = /^;+(.*)/; /* assumes that: 1) * is greedy; 2) . doesn't match \n */
 
 
+function getStringToken(string) {
+    var i, isStringEnded = false;
+    if( string[0] !== '"' ) {
+      throw new ParseError('string must begin with "', string[0]);
+    }
+
+    i = 1;
+    while( i < string.length ) {
+      if( string[i] === '"' ) {
+        isStringEnded = true;
+        break;
+      } else {
+        i += 1;
+      }
+    }
+    if( !isStringEnded ) {
+      throw new ParseError("tokenizer error: end-of-string (\") not found", string);
+    }
+    return {
+      'token': new Token('string', string.substring(1, i)),
+      'rest' : string.substring(i + 1)
+    };
+}
+
+
+// String -> Maybe (Token, String)
+//   where false is the "empty" value
 function nextToken(string) {
   var match;
   
@@ -15,25 +64,43 @@ function nextToken(string) {
 
   // 1. empty string
   if( !string ) {
-    return {
-      token: false,
-      rest: false
-    };
+    return false;
   } 
   
-  // 2. first char is '(' or ')'
-  if( string[0] === OPEN || string[0] === CLOSE ) {
+  // 2. first char is '('
+  if( string[0] === OPEN ) {
     return {
-      token: string[0],
-      rest: string.substring(1)
+      'token': new Token('open', string[0]),
+      'rest' : string.substring(1)
     };
   } 
+
+  // 3. first char is ')'
+  if( string[0] === CLOSE ) {
+    return {
+      'token': new Token('close', string[0]),
+      'rest' : string.substring(1)
+    };
+  } 
+
+  // 4. comment
+  if( match = string.match(COMMENT) ) {
+    return {
+      'token': new Token('comment', match[1]),
+      'rest' : string.substring(match[0].length)
+    };
+  }
+
+  // 5. string
+  if( string[0] === '"' ) {
+    return getStringToken(string);
+  }
   
-  // 3. leading chars not in { whitespace or '(' or ')' }
+  // 6. symbol
   match = string.match(SYMBOL);
   return {
-    token: match[0],
-    rest: string.substring(match[0].length)
+      'token': new Token('symbol', match[0]),
+      'rest' : string.substring(match[0].length)
   };
 }
 
@@ -43,7 +110,7 @@ function tokenize(string) {
       next;
   while( 1 ) {
     next = nextToken(string);
-    if( next.token ) {
+    if( next ) {
       tokens.push(next.token);
       string = next.rest;
     } else {
@@ -54,46 +121,19 @@ function tokenize(string) {
 }
 
 
-function getSymbol(tokens) {
+// [Token] -> Maybe SExpression
+//   returns false if token stream is empty or first token is not a symbol or string
+//   returns false if first token is a comment
+function getAtom(tokens) {
   if( tokens.length === 0) {
-	  return false;
+    return false;
   }
 	
   var first = tokens[0];
-  
-  if( first === OPEN || first === CLOSE ) {
-    return false;
-  }
-  
-  return {
-    result: first,
-    rest: tokens.slice(1)
-  };
-}
 
-
-function getList(tokens) {
-  var sexpr, elems;
-  
-  if( tokens[0] !== OPEN ) {
-    return false;
-  }
-
-  elems = [];
-  tokens = tokens.slice(1);
-  
-  while( tokens[0] && (tokens[0] !== CLOSE) ) {
-    sexpr = getSExpression(tokens);
-    if(!sexpr) {
-      return false;
-    }
-    elems.push(sexpr.result);
-    tokens = sexpr.rest;
-  }
-
-  if( tokens[0] === CLOSE ) {
+  if( first.type === 'symbol' || first.type === 'string' ) {
     return {
-      result: elems,
+      result: new SExpression(first.type, first.value),
       rest: tokens.slice(1)
     };
   }
@@ -102,11 +142,49 @@ function getList(tokens) {
 }
 
 
+// [Token] -> Maybe SExpression
+//   returns false if tokens is empty or doesn't start with open
+//   returns false if a properly 'balanced' list can't be found
+function getList(tokens) {
+  var sexpr, 
+      elems = [];
+  
+  // a list *has* to start with a '('
+  if( tokens.length === 0 || tokens[0].type !== 'open' ) {
+    return false;
+  }
+
+  tokens = tokens.slice(1);
+  
+  // keep going until a ')'
+  while( tokens[0] && (tokens[0].type !== 'close') ) {
+    // a list could have as many nested lists or atoms as it pleased
+    sexpr = getSExpression(tokens);
+    if( !sexpr ) {
+      return false;
+    }
+    elems.push(sexpr.result);
+    tokens = sexpr.rest;
+  }
+
+  // a list needs a close-paren
+  if( tokens[0] && tokens[0].type === 'close' ) {
+    return {
+      result: new SExpression('list', elems),
+      rest: tokens.slice(1)
+    };
+  }
+  
+  // uh-oh!  we didn't find a close-paren ...
+  return false;
+}
+
+
 function getSExpression(tokens) {
   var sexpr;
 
   // an s-expression is either a symbol
-  sexpr = getSymbol(tokens);
+  sexpr = getAtom(tokens);
   if( sexpr ) {
     return sexpr;
   }
@@ -122,58 +200,32 @@ function getSExpression(tokens) {
 }
 
 
-function parseOne(string) {
+// String -> Maybe [SExpression]
+//   returns false if, after all s-expressions are extracted,
+//   there are still tokens left in the token stream
+function parse(string) {
   var tokens = tokenize(string),
+      sexprs = [],
       sexpr;
-
-  if( tokens.length === 0 ) {
-    return false;
+	
+  while( sexpr = getSExpression(tokens) ) {
+    sexprs.push(sexpr.result);
+    tokens = sexpr.rest;
   }
-
-  sexpr = getSExpression(tokens);
-  
-  // got an s-expression and nothing but an s-expression
-  if( sexpr && (sexpr.rest.length === 0) ) {
-    return sexpr.result;
-  } 
-  
-  // got an s-expression but there was stuff after it
-  if ( sexpr ) {
-    return false;
-  } 
-  
-  // couldn't get an s-expression
-  return false;
-}
-
-
-function parseAll(string) {
-	var tokens = tokenize(string),
-	    sexprs = [],
-	    sexpr;
 	
-	while( sexpr = getSExpression(tokens) ) {
-		sexprs.push(sexpr.result);
-		tokens = sexpr.rest;
-	}
-	
-	if( tokens.length !== 0 ) {
-		return false;
-	}
-	
-	return {
-		'sexprs': sexprs
-	};
+  if( tokens.length !== 0 ) {
+    throw new ParseError('unconsumed tokens remaining after parse', tokens);
+  }
+	 
+  return sexprs;
 }
 
 
 return {
-  // [Token] -> ParseResult
-  'getSymbol'      : getSymbol,
+  'getAtom'        : getAtom,
   'getList'        : getList,
   'getSExpression' : getSExpression,
-  'parse'          : parseOne,
-  'parseAll'       : parseAll,
+  'parse'          : parse,
   'nextToken'      : nextToken,
   'tokenize'       : tokenize
 }
--- src/javascript/test_parse.js
+++ src/javascript/test_parse.js
@@ -1,10 +1,30 @@
 
 function testParse(lang) {
 
+
+function asJson(obj) {
+  var out;
+  if( typeof(obj) === 'object' && obj.length !== undefined ) {
+    out = [];
+    for(var i = 0; i < obj.length; i++) {
+      out.push(asJson(obj[i]));
+    }
+  } else if( typeof(obj) === 'object' ) {
+    out = {};
+    for(var key in obj) {
+      out[key] = asJson(obj[key]);
+    }
+  } else { // it's a number/string
+    out = obj;
+  }
+  return out;
+}
+
+
     module("tokenizer");
     
     test("nextToken", function() {
-    	expect(11);
+    	expect(15);
 
       var open = "  ((duh",
           close = ") what's going on",
@@ -15,27 +35,33 @@ function testParse(lang) {
           wsstr = ' "ab cd" f';
 
       var o = lang.nextToken(open);
-      equal('(', o.token);
+      equal('(', o.token.value);
+      equal('open', o.token.type);
       equal('(duh', o.rest);
 
       var c = lang.nextToken(close);
-      equal(')', c.token);
+      equal(')', c.token.value);
+      equal('close', c.token.type);
       equal(" what's going on", c.rest);
 
       var s = lang.nextToken(symbol);
-      equal('abc', s.token);
+      equal('abc', s.token.value);
+      equal('symbol', s.token.type);
       equal(')(()', s.rest);
 
       var n = lang.nextToken(number);
-      equal('12345', n.token);
+      equal('12345', n.token.value);
+      equal('symbol', n.token.type);
       equal(' )', n.rest);
 
       var p = lang.nextToken(empty);
-      deepEqual({token: false, rest: false}, p);
+      equal(false, p, "empty string");
       
-      deepEqual({token: '"abc"', rest: ""}, lang.nextToken(str));
+      var q = lang.nextToken(str);
+      deepEqual([q.token.type, q.token.value, q.rest], ['string', 'abc', '']);
       
-      deepEqual({token: '"ab cd"', rest: "f"}, lang.nextToken(wsstr));
+      var r = lang.nextToken(wsstr);
+      deepEqual([r.token.type, r.token.value, r.rest], ['string', 'ab cd', " f"]);
     });
 
     test("tokenize", function() {
@@ -46,18 +72,20 @@ function testParse(lang) {
           s5 = "(+ 1 2 (+ 3 4 (+ 5 (/ 6 7))))",
           s6 = "";
 
-      deepEqual(["(", "abc", ")"], lang.tokenize(s1));
+      var getVal = function (t) {return t.value;};
 
-      deepEqual(["(", "(", "(", "(", "(", "(", "("], lang.tokenize(s2));
+      deepEqual(["(", "abc", ")"], lang.tokenize(s1).map(getVal));
 
-      deepEqual([")", ")", ")", ")", "(", "(", "("], lang.tokenize(s3));
+      deepEqual(["(", "(", "(", "(", "(", "(", "("], lang.tokenize(s2).map(getVal));
 
-      deepEqual(["abc123", "abc", "(", "der", ")"], lang.tokenize(s4));
+      deepEqual([")", ")", ")", ")", "(", "(", "("], lang.tokenize(s3).map(getVal));
+
+      deepEqual(["abc123", "abc", "(", "der", ")"], lang.tokenize(s4).map(getVal));
 
       deepEqual(["(", "+", "1", "2", "(", "+", "3", "4",
                  "(", "+", "5", "(", "/", "6", "7", ")",
                  ")", ")", ")"], 
-          lang.tokenize(s5)
+          lang.tokenize(s5).map(getVal)
       );
 
       deepEqual([], lang.tokenize(s6));
@@ -67,134 +95,312 @@ function testParse(lang) {
 
     module("parse");
     
-    test("getSymbol", function() {
+    test("getAtom", function() {
 
-      var t1 = ["(", "duh"],
-          t2 = ["123abc", "(", 'barf'],
-          t3 = [")", "abc", "("],
-          t4 = [];
+      var t1 = [{'type': 'open', 'value': "("}, {'type': 'symbol', 'value': "duh"}],
+          t2 = [{'type': 'symbol', 'value': "123abc"}, 
+                {'type': 'open', 'value': "("},
+                {'type': 'symbol', 'value': 'barf'}
+          ],
+          t3 = [
+            {'type': 'close', 'value': ")"}, 
+            "abc", 
+            {'type': 'open', 'value': "("}
+          ],
+          t4 = [],
+          com = [{'type': 'comment', 'value': 'blargh'}],
+          str = [{'type': 'string', 'value': 'me!'}];
 
-      var o = lang.getSymbol(t1);
-      equal(false, o, "( is not a symbol");
+      var o = lang.getAtom(t1);
+      equal(false, o, "( is not an atom");
 
-      var p = lang.getSymbol(t2);
-      equal("123abc", p.result, "symbol");
-      deepEqual(["(", 'barf'], p.rest, "rest of token stream");
+      var p = lang.getAtom(t2);
+      equal("123abc", p.result.value, "symbols are atoms");
+      deepEqual(t2.slice(1), p.rest, "rest of token stream");
 
-      var q = lang.getSymbol(t3);
-      equal(false, q, ") is not a symbol");
+      var q = lang.getAtom(t3);
+      equal(false, q, ") is not an atom");
       
-      equal(false, lang.getSymbol(t4), "can't find symbol empty list");
+      equal(false, lang.getAtom(t4), "no atom in empty list");
+
+      equal(false, lang.getAtom(com), "comments are not atoms");
+
+      var z = lang.getAtom(str).result;
+      deepEqual(str[0], {'type': z.type, 'value': z.value}, "strings are atoms");
     });
 
     test("getList", function() {
-      expect(12);
-      var t1 = [")"],
-          t2 = ["(", ")"],
-          t3 = ["(", "abc", ")", "def"],
-          t4 = ["(", "1", "(", "+", ")", "2", ")", "(", "rest!"],
-          t5 = ["abc"],
+      expect(13);
+      function m(t) {
+          var o = {'value': t};
+          if( t === '(' )      {o.type = 'open'; }
+          else if( t === ')' ) {o.type = 'close';}
+          else { o.type = 'symbol';}
+          return o;
+      }
+
+      var t1 = [")"].map(m),
+          t2 = ["(", ")"].map(m),
+          t3 = ["(", "abc", ")", "def"].map(m),
+          t4 = ["(", "1", "(", "+", ")", "2", ")", "(", "rest!"].map(m),
+          t5 = ["abc"].map(m),
           t6 = [],
-          t7 = ["(", "abc", "(", "-", ")", "ohnoes"],
-          t8 = ["(", "(", "(", "(", ")", ")", ")", ")", "12345"],
-          t9 = ["(", "(", "(", "(", ")", ")", ")"];
+          t7 = ["(", "abc", "(", "-", ")", "ohnoes"].map(m),
+          t8 = ["(", "(", "(", "(", ")", ")", ")", ")", "12345"].map(m),
+          t9 = ["(", "(", "(", "(", ")", ")", ")"].map(m);
+
+      var getVal = function (t) {return t.value;};
 
       var o = lang.getList(t1);
-      equal(false, o);
+      equal(false, o, "list needs an open as well as a close");
 
       var p = lang.getList(t2);
-      deepEqual([], p.result);
+      deepEqual([], p.result.value);
+      equal("list", p.result.type);
 
-      var q = lang.getList(t3);
-      deepEqual(["abc"], q.result);
-      deepEqual(["def"], q.rest);
+      var q = lang.getList(t3),
+          q1 = q.result.value[0];
+      deepEqual({'type': 'symbol', 'value': 'abc'}, {'type': q1.type, 'value': q1.value});
+      deepEqual(["def"], q.rest.map(getVal));
 
-      var r = lang.getList(t4);
-      deepEqual(["1", ["+"], "2"], r.result);
-      deepEqual(["(", "rest!"], r.rest);
+      var r = lang.getList(t4),
+          r1 = r.result.value;
+// [{'type': 'symbol', 'value': "1"}, ["+"], "2"]
+      deepEqual(['list', 'symbol', 'list', 'symbol'], [r.result.type, r1[0].type, r1[1].type, r1[1].value[0].type]);
+      deepEqual(["(", "rest!"].map(m), r.rest);
 
       var s = lang.getList(t5);
-      equal(false, s);
+      equal(false, s, "can't get list from symbol");
 
       var t = lang.getList(t6);
-      equal(false, t);
+      equal(false, t, "can't get list from empty token stream");
 
       var u = lang.getList(t7);
-      equal(false, u);
+      equal(false, u, "can't get list if missing close-paren");
       
       var v = lang.getList(t8);
-      deepEqual([[[[]]]], v.result);
-      deepEqual(["12345"], v.rest);
+      deepEqual({
+          "type": "list",
+          "value": [{
+              "type": "list",
+              "value": [{
+                  "type": "list",
+                  "value": [{
+                      "type": "list",
+                      "value": []
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }, asJson(v.result));
+      deepEqual([{'type': 'symbol', 'value': "12345"}], v.rest, "rest of deeply nested list");
       
       var w = lang.getList(t9);
       equal(false, w);
     });
 
-    test("parseOne", function() {
-      expect(7);
+
+    test("parse", function() {
+      expect(9);
       var p1 = "(+ 3 2)",
           p2 = "3 4",
           p3 = "(+ (- (* 4 5 (abc (quote def)) 17)))",
           p4 = "(+ (- (* 4 5 (abc (quote def)) 17))",
           p5 = "()",
           p6 = "abc",
-          p7 = "";
+          p7 = "",
+          p8 = "(define x 4) \n (define y 5)",
+          p9 = "abc 1 2 3 (duh)";
 
       var a = lang.parse(p1);
-      deepEqual(["+", "3", "2"], a);
+      deepEqual([
+        {
+          "type": "list",
+          "value": [
+            {
+              "type": "symbol",
+              "value": "+"
+            },
+            {
+              "type": "symbol",
+              "value": "3"
+            },
+            {
+              "type": "symbol",
+              "value": "2"
+            }
+          ]
+        }
+      ], asJson(a));
 
       var b = lang.parse(p2);
-      equal(false, b);
+      deepEqual([{
+            "type": "symbol",
+            "value": "3"
+          },
+          {
+            "type": "symbol",
+            "value": "4"
+          }
+        ], asJson(b));
 
       var c = lang.parse(p3);
       deepEqual([
-        "+", 
-         ["-", 
-          ["*", "4", "5", 
-           ["abc", 
-            ["quote", "def"]], 
-           "17"]]], 
-      c);
+  {
+    "type": "list",
+    "value": [
+      {
+        "type": "symbol",
+        "value": "+"
+      },
+      {
+        "type": "list",
+        "value": [
+          {
+            "type": "symbol",
+            "value": "-"
+          },
+          {
+            "type": "list",
+            "value": [
+              {
+                "type": "symbol",
+                "value": "*"
+              },
+              {
+                "type": "symbol",
+                "value": "4"
+              },
+              {
+                "type": "symbol",
+                "value": "5"
+              },
+              {
+                "type": "list",
+                "value": [
+                  {
+                    "type": "symbol",
+                    "value": "abc"
+                  },
+                  {
+                    "type": "list",
+                    "value": [
+                      {
+                        "type": "symbol",
+                        "value": "quote"
+                      },
+                      {
+                        "type": "symbol",
+                        "value": "def"
+                      }
+                    ]
+                  }
+                ]
+              },
+              {
+                "type": "symbol",
+                "value": "17"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+], asJson(c));
 
-      var d = lang.parse(p4);
-      equal(false, d);
+      var d = false;
+      try {
+        lang.parse(p4);
+      } catch(e) {
+        d = true;
+      };
+      ok(d, "unbalanced parentheses cause parse to fail");
 
       var e = lang.parse(p5);
-      deepEqual([], e);
+      deepEqual([{
+        "type": "list",
+        "value": []
+      }], asJson(e));
 
       var f = lang.parse(p6);
-      equal("abc", f);
-
+      deepEqual([{
+        "type": "symbol",
+        "value": "abc"
+      }], asJson(f));
+  
       var g = lang.parse(p7);
-      equal(false, g);
+      deepEqual(g, [], "empty string results in empty list");
 
-    });
+      deepEqual(	
 
-    test("parseAll", function() {
-      expect(7);
-      var p1 = "(+ 3 2)",
-          p2 = "3 4",
-          p3 = "(+ (- (* 4 5 \t 17)))",
-          p4 = "(define x 4) \n (define y 5)",
-          p5 = "()",
-          p6 = "abc 1 2 3 (duh)",
-          p7 = "";
-      
-      deepEqual({'sexprs':[["+", "3", "2"]]}, lang.parseAll(p1));
-      
-      deepEqual({'sexprs':["3", "4"]}, lang.parseAll(p2));
-      
-      deepEqual({'sexprs':[["+", ["-", ["*", "4", "5", "17"]]]]}, lang.parseAll(p3));
-      
-      deepEqual({'sexprs':[["define", "x", "4"], ["define", "y", "5"]]}, lang.parseAll(p4));
-      
-      deepEqual({'sexprs':[[]]}, lang.parseAll(p5));
-      
-      deepEqual({'sexprs':["abc", "1", "2", "3", ["duh"]]}, lang.parseAll(p6));
-      
-      deepEqual({'sexprs':[]}, lang.parseAll(p7));
-      
+[
+  {
+    "type": "list",
+    "value": [
+      {
+        "type": "symbol",
+        "value": "define"
+      },
+      {
+        "type": "symbol",
+        "value": "x"
+      },
+      {
+        "type": "symbol",
+        "value": "4"
+      }
+    ]
+  },
+  {
+    "type": "list",
+    "value": [
+      {
+        "type": "symbol",
+        "value": "define"
+      },
+      {
+        "type": "symbol",
+        "value": "y"
+      },
+      {
+        "type": "symbol",
+        "value": "5"
+      }
+    ]
+  }
+], asJson(lang.parse(p8)));
+            
+      deepEqual([
+  {
+    "type": "symbol",
+    "value": "abc"
+  },
+  {
+    "type": "symbol",
+    "value": "1"
+  },
+  {
+    "type": "symbol",
+    "value": "2"
+  },
+  {
+    "type": "symbol",
+    "value": "3"
+  },
+  {
+    "type": "list",
+    "value": [
+      {
+        "type": "symbol",
+        "value": "duh"
+      }
+    ]
+  }
+], asJson(lang.parse(p9)));
+
     });
 
 }
-
