0c4ad38fe495714992689e5349d6429e392d80d7
Matt Fenwick <mfenwick100@gmail.com>
2012-08-14 06:58:09 -0400

    refactor apply/eval to handle error conditions

--- src/haskell/Evaluate.hs
+++ src/haskell/Evaluate.hs
@@ -4,6 +4,7 @@ module Evaluate (
 
 import Parser (ASTNode(..), full) -- as Parser
 import qualified Data.Map as Map
+import Control.Monad (foldM, liftM)
 
 
 -- --------------------------------------------------------
@@ -16,42 +17,76 @@ data LispVal
   | LList [LispVal]
   | LChar Char
   | LBoolean Bool
-  | LFunc ([LispVal] -> LispVal)
-  | LSpecial (Environment -> [ASTNode] -> LispVal)
+  | LNull
+  | LFunc ([LispVal] -> Either String LispVal)
+  | LSpecial (Environment -> [ASTNode] -> Either String (Environment, LispVal))
   
   
 instance Show LispVal where
-  show (LNumber f) = show f
-  show (LList fs) = show $ map show fs
-  show (LChar c) = show c
-  show (LFunc f) = "function"
+  show (LNumber f)  = show f
+  show (LList fs)   = show $ map show fs
+  show (LChar c)    = show c
+  show (LFunc f)    = "function"
   show (LBoolean b) = show b
+  show LNull        = "<nil>"
+  show (LSpecial s) = "special form"
+
+
+applySpecial :: Environment 
+  -> (Environment -> [ASTNode] -> Either String (Environment, LispVal)) 
+  -> [ASTNode] 
+  -> Either String (Environment, LispVal)
+applySpecial e s form = s e form
+
+{- -- needs Eq instance or something ... odd
+define :: Environment -> [ASTNode] -> Either String (Environment, LispVal)
+define e [ASymbol sym, val]
+  | Map.lookup sym e == Nothing = eval e val >>=
+                                  (\(e1, v) -> let e2 = Map.insert sym v
+                                               in return (e2, LNull))
+  | otherwise = Left ("symbol " ++ sym ++ " cannot be rebound")
+  -}
+
+cons :: [LispVal] -> Either String LispVal
+cons [e, LList es] = Right $ LList (e:es)
+cons [_, x] = Left ("type error in 'cons': expected list, got " ++ (show x))
+cons _ = Left "NumArgsError in 'cons'"
+
   
-  
-apply :: Environment -> LispVal -> [LispVal] -> Either String (Environment, LispVal)
-apply e (LFunc f) args = Right (e, f args)
-apply e (LSpecial s) forms = Left "special form evaluation is unimplemented"
+applyFunction :: Environment
+  -> ([LispVal] -> Either String LispVal)
+  -> [ASTNode]
+  -> Either String (Environment, LispVal)
+applyFunction e f forms = evalForms e forms >>= 
+  (\(e1, vals) -> f vals >>=
+  (\result -> return (e1, result)))
+
+
+apply :: Environment -> LispVal -> [ASTNode] -> Either String (Environment, LispVal)
+apply e (LFunc f) args = applyFunction e f args -- Right (e, f args)
+apply e (LSpecial s) forms = s e forms
 apply _ _ _ = Left "hey, you didn't give me a function or special form!"
 
 
-evalForm :: Either String (Environment, [LispVal]) -> ASTNode -> Either String (Environment, [LispVal])
-evalForm base nextForm = base >>= (\(env, evaledForms) -> 
-  eval env nextForm >>= (\(nextEnv, evaledForm) -> 
-  return (nextEnv, evaledForm : evaledForms)))
+evalForm :: (Environment, [LispVal]) -> ASTNode -> Either String (Environment, [LispVal])
+evalForm (e0, vals) form = eval e0 form >>=
+  (\(e1, val) -> return (e1, val : vals))
+
+
+evalForms :: Environment -> [ASTNode] -> Either String (Environment, [LispVal])
+evalForms e fs = foldM evalForm (e, []) fs
   
   
 eval :: Environment -> ASTNode -> Either String (Environment, LispVal)
-eval e (ANumber f) = Right (e, LNumber f)
-eval e (AChar c) = Right (e, LChar c)
-eval e (ASymbol s) = (fromMaybe $ Map.lookup s e) >>= (\v -> return (e, v))
+eval e (ANumber f)  = Right (e, LNumber f)
+eval e (AChar c)    = Right (e, LChar c)
+eval e (ASymbol s)  = (fromMaybe $ Map.lookup s e) >>= (\v -> return (e, v))
   where fromMaybe Nothing = Left ("unbound variable: " ++ s)
         fromMaybe (Just x) = Right x
-eval e (AList fs) = foldl evalForm (Right (e, [])) fs >>= 
-  (\(newEnv, newForms) -> Right (newEnv, LList $ reverse newForms))
+eval e (AList fs)   = liftM (\(a, b) -> (a, LList $ reverse b)) $ evalForms e fs
 eval e (Application op args) = eval e op >>=
-  (\(e1, f) -> foldl evalForm (Right (e1, [])) args >>=
-  (\(newEnv, newForms) -> apply newEnv f $ reverse newForms))
-  
+  (\(e1, f) -> apply e1 f args)
+
   
 defaultEnv = Map.fromList [("true", LBoolean True), ("false", LBoolean False)]
 
