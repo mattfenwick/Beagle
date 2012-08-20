e26d9445bf117f2958d57d5609dd377810c49411
Matt Fenwick <mfenwick100@gmail.com>
2012-08-20 16:31:57 -0400

    add module for macros, including 'let'

---
+++ src/javascript/macros.js
@@ -0,0 +1,25 @@
+var Macros = (function(Parser) {
+    
+    function Let(bindings, body) {
+        this.bindings = bindings;
+        this.body = body;
+    }
+    
+    // Macro -> AST
+    Let.prototype.expand = function() {
+        // f :: [(Sym, Expr)] -> [Define]
+        function f(b) {
+            return Parser.Define(b.elements);//inding);
+        }
+        return Parser.Application([
+            Parser.Lambda([Parser.ASTList([])].concat(this.bindings.map(f), this.body))
+        ]);
+    }
+    
+    return {
+        'Let': function(bs, b) {
+            return new Let(bs, b);
+        }
+    };
+    
+})(Parser);
---
+++ src/test/test_macros.js
@@ -0,0 +1,32 @@
+function testMacros(macros, parser) {
+
+    module("macros");
+    
+    var list = parser.ASTList,
+        lam = parser.Lambda,
+        def = parser.Define,
+        sym = parser.Symbol,
+        app = parser.Application;
+    
+    test("let", function() {
+        
+        // {let [[a b]]
+        //   (+)}
+        //
+        // -- to --
+        //
+        // ({lambda []
+        //    {define a b}
+        //    (+)})
+        var i = macros.Let([Parser.ASTList([Parser.Symbol('a'), Parser.Symbol('b')])], 
+                           Parser.Application([Parser.Symbol('+')])).expand(),
+            o = app([
+                lam([list([]),
+                    def([sym('a'), sym('b')]),
+                    app([sym('+')])
+                ])
+            ]);
+        
+        deepEqual(i, o, "let-expansion");
+    });
+}
