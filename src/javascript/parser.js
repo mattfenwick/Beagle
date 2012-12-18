9a1c939b0e74be681f16fa8618f0e906468c5fc3
Matt Fenwick <mfenwick100@gmail.com>
2012-12-18 02:44:22 +0100

    refactor parser

--- src/javascript/parser.js
+++ src/javascript/parser.js
@@ -1,40 +1,32 @@
 var Parser = (function(P) {
 
-    function myObject(pairs, line, column) {
+    function myObject(pairs) {
         return {
             type     : 'objectliteral',
-            entries  :  pairs,
-            line     :  line,
-            column   :  column
+            entries  :  pairs
         };
     }
     
-    function myList(elements, line, column) {
+    function myList(elements) {
         return {
             type      :  'listliteral',
-            elements  :  elements,
-            line      :  line,
-            column    :  column
+            elements  :  elements
         };
     }
     
-    function myApp(op, args, line, column) {
+    function myApp(op, args) {
         return {
             'type'     :  'application',
             'operator' :  op,
-            'arguments':  args,
-            line       :  line,
-            column     :  column
+            'arguments':  args
         };
     }
     
-    function mySpecial(op, args, line, column) {
+    function mySpecial(op, args) {
         return {
             'type'     :  'special',
             'operator' :  op,
-            'arguments':  args,
-            line       :  line,
-            column     :  column
+            'arguments':  args
         };
     }
 
@@ -53,52 +45,10 @@ var Parser = (function(P) {
     var pString = tokentype('string');
 
     var pSymbol = tokentype('symbol');
-    
-    function myObject2(start) {
-        return function(pairs) {
-            return myObject(pairs, start.line, start.column);
-        };
-    }
-    
-    function myList2(start) {
-        return function(elems) {
-            return myList(elems, start.line, start.column);
-        };
-    };
-    
-    function mySpecial2(start) {
-        return function(op, args) {
-            return mySpecial(op, args, start.line, start.column);
-        };
-    }
-    
-    function myApp2(start) {
-        return function(op, args) {
-            return myApp(op, args, start.line, start.column);
-        };
-    }
 
     var pForm = P.Parser.error("need to set function");    
-    
-    var pObject = tokentype('open-curly').bind(
-        function(t) {
-            return P.Parser.all([pString, pForm])
-                .many0()
-                .fmap(myObject2(t))
-                .seq2L(tokentype('close-curly'))
-                .commit({line: t.line, column: t.column, rule: 'object literal'});
-        });
-    
-    var pList = tokentype('open-square').bind(
-        function(t) {
-            return pForm 
-                .many0()
-                .fmap(myList2(t))
-                .seq2L(tokentype('close-square'))
-                .commit({line: t.line, column: t.column, rule: 'list literal'});
-        });
-    
-    function whatEvs(start, middle, end, rule) {
+
+    function delimited(start, middle, end, rule) {
         return start.bind(
             function(t) {
                 return middle
@@ -108,36 +58,32 @@ var Parser = (function(P) {
             });
     };
     
-    var pApp = whatEvs(
+    var pObject = delimited(
+        tokentype('open-curly'),
+        P.Parser.all([pString, pForm]).many0().fmap(myObject),
+        tokentype('close-curly'),
+        'object literal');
+    
+    var pList = delimited(
+        tokentype('open-square'),
+        pForm.many0().fmap(myList),
+        tokentype('close-square'),
+        'list literal');
+    
+    var pApp = delimited(
         tokentype('open-paren'),
         P.Parser.app(myApp, pForm, pForm.many0()),
         tokentype('close-paren'),
         'application');
     
-    var pApp = tokentype('open-paren').bind(
-        function(t) {
-            return P.Parser.app(
-                    myApp2(t),
-                    pForm,
-                    pForm.many0())
-                .seq2L(tokentype('close-paren'))
-                .commit({line: t.line, column: t.column, rule: 'application'});
-        });
+    var pSpec = delimited(
+        tokentype('open-special'),
+        P.Parser.app(mySpecial, pForm, pForm.many0()),
+        tokentype('close-special'),
+        'special-form application');
     
-    var pSpec = tokentype('open-special').bind(
-        function(t) {
-            return P.Parser.app(
-                    mySpecial2(t),
-                    pForm,
-                    pForm.many0())
-                .seq2L(tokentype('close-special'))
-                .commit({line: t.line, column: t.column, rule: 'special-form application'});
-        });
-    
-    // written as a function instead of a `var` to allow mutual recursion,
-    //   and functions are hoisted (I believe)
+    // written this way to allow mutual recursion
     pForm.parse = P.Parser.any([pSpec, pApp, pList, pObject, pSymbol, pNumber, pString]).parse;
-    // will this work?  if not, switch back to a function maybe
     
     var parser = pForm.many0();
     
