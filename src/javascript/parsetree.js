0da4599c4750dae83dffc5a6c273d28eb952d08b
Matt Fenwick <mfenwick100@gmail.com>
2013-01-09 15:09:15 +0100

    ParseTree: module containing data definitions of parse tree node types; tests

---
+++ src/javascript/parsetree.js
@@ -0,0 +1,107 @@
+var ParseTree = (function() {
+    "use strict";
+    
+    function myNumber(num, meta) {
+        if(typeof num !== 'number') {
+            throw new Error('type error -- expected number');
+        }
+        return {
+            type     :  'parsenode',
+            nodetype :  'number',
+            value      :  num,
+            meta       :  meta
+        };
+    }
+    
+    function mySymbol(sym, meta) {
+        if(typeof sym !== 'string') {
+            throw new Error('type error -- expected string');
+        }
+        return {
+            type     :  'parsenode',
+            nodetype :  'symbol',
+            value      :  sym,
+            meta       :  meta
+        };
+    }
+    
+    function myString(str, meta) {
+        if(typeof str !== 'string') {
+            throw new Error('type error -- expected string');
+        }
+        return {
+            type      :  'parsenode',
+            nodetype  :  'string',
+            value     :  str,
+            meta      :  meta
+        };
+    }
+
+    function myObject(table, meta) {
+        if(typeof table !== 'object') {
+            throw new Error('type error -- expected object');
+        }
+        return {
+            type     :  'parsenode',
+            nodetype :  'objectliteral',
+            table    :  table,
+            meta     :  meta
+        };
+    }
+    
+    function myList(elements, meta) {
+        if(elements.length === undefined) {
+            throw new Error('type error -- expected array');
+        };
+        return {
+            type      :  'parsenode',
+            nodetype  :  'listliteral',
+            elements  :  elements,
+            meta      :  meta
+        };
+    }
+    
+    function myApp(op, args, meta) {
+        if(op.type !== 'parsenode') {
+            throw new Error('type error -- expected parsenode');
+        }
+        if(args.length === undefined) {
+            throw new Error('type error -- expected array');
+        }
+        return {
+            type       :  'parsenode',
+            nodetype   :  'application',
+            'operator' :  op,
+            'arguments':  args,
+            meta       :  meta
+        };
+    }
+    
+    function mySpecial(op, args, meta) {
+        if(typeof op !== 'string') {
+            throw new Error('type error -- expected string');
+        }
+        if(args.length === undefined) {
+            throw new Error('type error -- expected array');
+        }
+        return {
+            type       :  'parsenode',
+            nodetype   :  'special',
+            'operator' :  op,
+            'arguments':  args,
+            meta       :  meta
+        };
+    }
+
+
+    return {
+        'number'  :  myNumber,
+        'symbol'  :  mySymbol,
+        'string'  :  myString,
+        'app'     :  myApp,
+        'list'    :  myList,
+        'object'  :  myObject,
+        'special' :  mySpecial
+    };
+
+})();
---
+++ src/test/test_parsetree.js
@@ -0,0 +1,87 @@
+function testParseTree(pt, helper) {
+
+    module("parse tree");
+    
+    var exp = helper.expectException;
+
+    test("number", function() {
+        deepEqual({type: 'parsenode', nodetype: 'number', value: 13, meta: 'abc'}, pt.number(13, 'abc'));
+    
+        exp(function() {
+            pt.number('oops');
+        }, 'TypeError');
+    });
+
+    test("string", function() {
+        deepEqual({type: 'parsenode', nodetype: 'string', value: 'hi', meta: 4}, pt.string('hi', 4));
+    
+        exp(function() {
+            pt.string([1]);
+        }, 'TypeError');
+    });
+
+    test("symbol", function() {
+        deepEqual({type: 'parsenode', nodetype: 'symbol', value: 'hi', meta: '??'}, pt.symbol('hi', '??'));
+    
+        exp(function() {
+            pt.symbol({a: 4});
+        }, 'TypeError');
+    });
+
+    test("list literal", function() {
+        deepEqual({type: 'parsenode', nodetype: 'listliteral', elements: [1,2,3], meta: 'hi'}, pt.list([1,2,3], 'hi'));
+        
+        exp(function() {
+            pt.list(3);
+        }, 'TypeError');
+        
+        exp(function() {
+            pt.list('qrs');
+        }, 'TypeError', 'detect difference between array and string');
+    });
+
+    test("application", function() {
+        deepEqual(
+            {type: 'parsenode', nodetype: 'application', 
+             operator: pt.symbol('qrs'), 'arguments': [], meta: 2}, 
+            pt.app(pt.symbol('qrs'), [], 2)
+        );
+        
+        exp(function() {
+            pt.app(3, [], 2);
+        }, 'TypeError', 'application operator must be a parsetree node');
+        
+        exp(function() {
+            pt.app(pt.symbol('qrs'), 3);
+        }, 'TypeError', 'application arguments must be an array');
+    });
+
+    test("object", function() {
+        deepEqual(
+            {type: 'parsenode', nodetype: 'objectliteral',
+             table: {'abc': 123}, meta: {q: 'rs'}},
+            pt.object({'abc': 123}, {q: 'rs'})
+        );
+        
+        exp(function() {
+            pt.object(123);
+        }, 'TypeError');
+    });
+
+    test("special form", function() {
+        deepEqual(
+            {type: 'parsenode', nodetype: 'special',
+             operator: 'cond', 'arguments': [], meta: 4},
+            pt.special('cond', [], 4)
+        );
+        
+        exp(function() {
+            pt.special({}, [], 41);
+        }, 'TypeError', 'special form operator must be a string');
+        
+        exp(function() {
+            pt.special('define', true, 'me');
+        }, 'TypeError', 'special form arguments must be an array');
+    });
+
+}
