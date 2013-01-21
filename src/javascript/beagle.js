3b896c5f2cb21dbfaf3570ed719bdcc915f33c09
Matt Fenwick <mfenwick100@gmail.com>
2013-01-21 21:45:36 +0100

    beagle interface:  allow access to environment

--- src/javascript/beagle.js
+++ src/javascript/beagle.js
@@ -49,6 +49,9 @@ var Beagle = (function (tokenizer, parser, evaluate) {
         return output;
     }
 
-    return beagle;
+    return {
+        exec:  beagle,
+        environment: env
+    };
 
 })(Tokenizer, PParser, Evaluate);
--- src/test/test_beagle.js
+++ src/test/test_beagle.js
@@ -6,16 +6,17 @@ function testBeagle(beagle, data, tokens, ast) {
     var list = data.List,
         num = data.Number,
         str = data.makeCharList,
-        t = tokens.Token;
+        t = tokens.Token,
+        exec = beagle.exec;
         
     test("tokenization error", function() {
-        var r = beagle('123 ",doit now');
+        var r = exec('123 ",doit now');
         deepEqual('token error', r.status);
         deepEqual({column: 5, line: 1, rest: '",doit now', message: 'end-of-string not found'}, r.tokenization.error);
     });
     
     test("parse error", function() {
-        var r = beagle('123\n [3');
+        var r = exec('123\n [3');
         deepEqual([t('integer', '123', {line: 1, column: 1}), t('whitespace', '\n ', {line: 1, column: 4}), 
                    t('open-square', '[', {line: 2, column: 2}), t('integer', '3', {line: 2, column: 3})], r.tokenization);
         deepEqual('parse error', r.status);
@@ -23,7 +24,7 @@ function testBeagle(beagle, data, tokens, ast) {
     });
     
     test("execution error", function() {
-        var r = beagle('1 x');
+        var r = exec('1 x');
         deepEqual([t('integer', '1', {line: 1, column: 1}), t('whitespace', ' ', {line: 1, column: 2}),
                    t('symbol', 'x', {line: 1, column: 3})], r.tokenization);
         deepEqual('execution error', r.status);
@@ -31,7 +32,7 @@ function testBeagle(beagle, data, tokens, ast) {
     });
     
     test("successful execution", function() {
-        var r = beagle('3 (+ 3 2) ; qrs \n "ab"');
+        var r = exec('3 (+ 3 2) ; qrs \n "ab"');
         deepEqual('success', r.status);
         deepEqual([ast.number(3, {line: 1, column: 1}), 
                    ast.application(ast.symbol('+', {line: 1, column: 4}), 
