d60c65a2fb2369e09fe953e69e06978a55cc1364
Matt Fenwick <mfenwick100@gmail.com>
2013-01-22 00:16:54 +0100

    unit tests for data module

--- src/test/test_data.js
+++ src/test/test_data.js
@@ -2,7 +2,32 @@ function testData(data) {
 	
 	module("data");
 	
-	test("should fail", function() {
-		ok(0, "unimplemented");
+	test("number", function() {
+		propEqual({type: 'number', value: 3}, data.Number(3));
 	});
+    
+    test("char", function() {
+        propEqual({type: 'char', value: 'a'}, data.Char('a'));
+    });
+    
+    test("list", function() {
+        propEqual({type: 'list', value: [data.Number(4)]}, data.List([data.Number(4)]));
+    });
+    
+    test("function", function() {
+        propEqual({argTypes: ['string'], name: 'alert', type: 'function', value: alert}, data.Function(['string'], 'alert', alert));
+    });
+    
+    test("null", function() {
+        propEqual({type: 'null'}, data.Null());
+    });
+    
+    test("boolean", function() {
+        propEqual({type: 'boolean', value: true}, data.Boolean(true));
+    });
+    
+    test("object", function() {
+        propEqual({type: 'object', value: {abc: 123}}, data.Object({abc: 123}));
+    });
+
 }
