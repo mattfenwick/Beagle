80cbdb4ccdd740c197fbfee4f61c9407b1373eda
Matt Fenwick <mfenwick100@gmail.com>
2013-01-21 21:46:27 +0100

    get REPL working again (partially):  load correct scripts, use new Beagle data interface, don't bother loading Beagle source files

--- src/repl/repl.js
+++ src/repl/repl.js
@@ -1,46 +1,30 @@
-
 var Repl = (function(_, Backbone, Beagle) {
-"use strict";
+    "use strict";
 
-
-function Evaluator() {
-  this._history = [];
-  this._lisp = Beagle.exec;
-  this._environment = Beagle.environment;
-}
-
-
-Evaluator.prototype.evalString = function(str) {
-  var evaled, obj;
-  try {
-    evaled = this._lisp(str);
-    if( evaled.result.length !== 1) {
-      throw new Error("needed exactly one form (found " + evaled.result.length + ")");
+    function Evaluator() {
+        this._history = [];
+        this._lisp = Beagle.exec;
+        this._environment = Beagle.environment;
     }
-    this._history.push(evaled);
-    this.trigger("success", evaled);
-  } catch(e) {
-    obj = {
-      'string': str,
-      'error': e
+
+    Evaluator.prototype.evalString = function(str) {
+        var evaled = this._lisp(str);
+        this._history.push(evaled);
+        if(evaled.status === 'success') {
+            this.trigger("success", evaled);
+        } else {
+            this.trigger("error", evaled);
+        }
+    }
+
+    Evaluator.prototype.getEnvironment = function() {
+        return this._environment;
+    }
+
+    _.extend(Evaluator.prototype, Backbone.Events);
+
+    return {
+        'Evaluator': function() {return new Evaluator();}
     };
-    this._history.push(obj);
-    this.trigger("error", obj);
-    throw e; // give the browser the chance to do something with it
-  };
-}
-
-
-Evaluator.prototype.getEnvironment = function() {
-  return this._environment;
-}
-
-
-_.extend(Evaluator.prototype, Backbone.Events);
-
-
-return {
-  'Evaluator': function() {return new Evaluator();}
-};
 
 })(_, Backbone, Beagle);
