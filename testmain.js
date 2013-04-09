5c0c39c59328e2e5df9d570a55e0220d43596454
Matt Fenwick <mfenwick100@gmail.com>
2013-04-09 09:27:17 -0400

    new web app build

---
+++ testmain.js
@@ -0,0 +1,1 @@
+var NEW_TESTS=["test/ast","test/tokens","test/environment","test/data","test/functions","test/tokenizer","test/parser","test/evaluate","test/beagle"];require(NEW_TESTS,function(){module("testmain"),test("testmain",function(){ok(1,"modules loaded")})});
