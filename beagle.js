63a9439c8fcddf51faa81302761d34bf7ac93ebf
Matt Fenwick <mfenwick100@gmail.com>
2015-04-23 21:18:54 -0500

    wire unparser into frontend

--- beagle.js
+++ beagle.js
@@ -1,14 +1,20 @@
-#! node
-
 'use strict';
 
 var frontend = require('./lib/frontend'),
+    unparser = require('./lib/unparser'),
     util = require('util'),
     fs = require('fs');
 
 var commands = {
     'parse'     : frontend.parse,
-    'parseAST'  : frontend.parseAST
+    'parseAST'  : frontend.parseAST,
+    'unparse'   : function (input) {
+                      var astErr = frontend.parseAST(input);
+                      if (astErr.status !== 'success') {
+                          return astErr;
+                      }
+                      return unparser.unparse(astErr.value);
+                  }
 };
 
 // console.log('args -- ' + process.argv);
@@ -26,6 +32,10 @@ var input = fs.readFileSync('/dev/stdin', {'encoding': 'utf8'});
 
 var output = f(input);
 
-process.stdout.write(util.inspect(output, {depth: null}) + "\n");
+if (typeof output === 'string') {
+    process.stdout.write(output + "\n");
+} else {
+    process.stdout.write(util.inspect(output, {depth: null}) + "\n");
+}
 // process.stdout.write(JSON.stringify(output, null, 2) + "\n");
 
